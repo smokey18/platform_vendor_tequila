59e5887e360923c8222d25e6e6bc02a98fa524c3
LibXZR <i@xzr.moe>
2022-05-08 01:01:02 +0800

    vendor: Add support for hiding navigation bar under screen keyboard [3/3]
    Change-Id: Ia808b96e850a1ce859e35af8b89d75d30e34e308
    Signed-off-by: LibXZR <i@xzr.moe>

--- config/common.mk
+++ config/common.mk
@@ -6,6 +6,7 @@ PRODUCT_BRAND ?= tequilaOS
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_PACKAGES += \
+    KeyboardNoNavigationBarOverlay \
     NavigationBarNoHintOverlay
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
